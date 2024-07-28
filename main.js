14d319bf249980a24aeda52dc6e8e85bd3aa3ac4
pbrotman <parkerbrotman@gmail.com>
2024-07-28 17:01:54 -0400

    Highlight header buttons on hover.

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-// Set current year for copyright date
+// Set current year for copyright date.
 let elsCurYear = document.querySelectorAll(".current-year");
-elsCurYear.forEach((el) => {
+elsCurYear.forEach(el => {
     el.innerHTML = new Date().getFullYear();
+})
+
+// Highlight header buttons on hover.
+let headerButtons = document.querySelectorAll("header button");
+headerButtons.forEach(el => {
+    el.addEventListener("mouseover", () => {el.classList.add("emphasis")});
+    el.addEventListener("mouseout", () => {el.classList.remove("emphasis")});
 })
