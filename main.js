7acf5a9ff6ccf65a7b1eec342a08ab4ec4cea554
pbrotman <parkerbrotman@gmail.com>
2024-07-30 09:55:14 -0400

    Toy with balloon pic styling.

--- main.js
+++ main.js
@@ -18,3 +18,8 @@ headerButtons.forEach(el => hoverEmphasis(el));
 
 let iconImgs = document.querySelectorAll("#icons i");
 iconImgs.forEach(el => hoverEmphasis(el, 3));
+
+// //Toy with balloon pic size
+// let balloonLaos = document.querySelector("#balloon-laos");
+// balloonLaos.setAttribute("width", 3024/8);
+// balloonLaos.setAttribute("height", 4032/8);
