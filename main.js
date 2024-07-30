040657a1ca4dce1a8d20b921eba1d298ef9653f7
pbrotman <parkerbrotman@gmail.com>
2024-07-30 09:38:53 -0400

    Add LinkedIn and Github icons to intro.

--- main.js
+++ main.js
@@ -2,17 +2,19 @@
 let elsCurYear = document.querySelectorAll(".current-year");
 elsCurYear.forEach(el => {
     el.innerHTML = new Date().getFullYear();
-})
+});
 
-// Highlight header buttons on hover.
-let headerButtons = document.querySelectorAll("header button");
-headerButtons.forEach(el => {
+// Highlight specific elements on hover.
+function hoverEmphasis(el){
     el.addEventListener("mouseover", () => {el.classList.add("emphasis")});
     el.addEventListener("mouseout", () => {el.classList.remove("emphasis")});
-})
+};
+
+let headerButtons = document.querySelectorAll("header button");
+headerButtons.forEach(el => hoverEmphasis(el));
 
 let projectDivs = document.querySelectorAll(".project");
-projectDivs.forEach(el => {
-    el.addEventListener("mouseover", () => {el.classList.add("emphasis")});
-    el.addEventListener("mouseout", () => {el.classList.remove("emphasis")});
-})
+projectDivs.forEach(el => hoverEmphasis(el));
+
+let iconImgs = document.querySelectorAll("#icons i");
+iconImgs.forEach(el => hoverEmphasis(el));
